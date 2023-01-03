4616569aa059d8f44ac20155ee59d8503b5c5fc7
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2023-01-03 19:12:52 +0400

    Add project files.

---
+++ Khanut_Iskakan/Class1.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khanut_Iskakan
+{
+    public class Class1
+    {
+        private readonly string _HASCE_FILEI = $"{Environment.CurrentDirectory}\\Khanut.json";
+        private string[] _names;
+        public void MyMethod(int a)
+        {
+            try
+            {
+                var data = File.ReadAllText(_HASCE_FILEI);
+                _names = JsonConvert.DeserializeObject<string[]>(data);
+                Console.WriteLine(_names);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine(ex.Message);
+
+            }
+
+        }
+    }
+}
---
+++ Khanut_Iskakan/Program.cs
@@ -0,0 +1,11 @@
+﻿using Khanut_Iskakan;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Class1 class1= new Class1();
+        class1.MyMethod(2);
+
+    }
+}
